a37b4aab7f0933aa925d5d3abac690b97a89b2ff
tiewehong <37630980+tiewehong@users.noreply.github.com>
2025-03-29 18:45:59 +1100

    Branch Commit 2: Fixed datetime issue, updated short & long MA, MA crossover, RSI calculation functions. Added decorator

--- SP_backend.py
+++ SP_backend.py
@@ -4,15 +4,27 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import date
 from dateutil.relativedelta import relativedelta
+import re
 import streamlit as st
+
+# Decorator function
+def safe_copy(func):
+    def wrapper(df, *args, **kwargs):
+        return func(df.copy(), *args, **kwargs)
+    return wrapper
 
 
 # Calculate required time frame
-def timeframe_default(start_date=None, end_date=None):
-    if start_date is None:
+def timeframe_default(start_date='', end_date=''):
+    if start_date == '':
         start_date = date.today() - relativedelta(years=10)
-    if end_date is None:
+    else:
+        date.fromisoformat(start_date)
+
+    if end_date == '':
         end_date = date.today()
+    else:
+        date.fromisoformat(start_date)
 
     return start_date, end_date
 
@@ -25,14 +37,16 @@ def timeframe_default(start_date=None, end_date=None):
 # Meaning: Measures if stock is overbought (>70) or oversold (<30).
 # Bullish: RSI moves above 30 (from oversold).
 # Bearish: RSI moves below 70 (from overbought).
-def calculate_short_term_RSI(df, window_length=14):
+@safe_copy
+def calculate_short_term_RSI(df, col='Close', window=14):
     # Calculate difference in price between each day
-    delta = df['Close'].diff()
+    delta = df[col].diff()
     # Calculate the gains and losses in ABSOLUTE terms
-    gain = (delta.where(delta > 0, 0)).rolling(window=window_length).mean()
-    loss = (-delta.where(delta < 0, 0)).rolling(window=window_length).mean()
+    gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
     rs = gain/loss
-    df['RSI'] = 100 - (100 / (1 + rs))
+    rsi = 100 - (100 / (1 + rs))
+    df[f'RSI_{window}days'] = rsi
     return df
 
 # Stochastic Oscillator
@@ -53,8 +67,11 @@ def calculate_VWAP(df):
 # Meaning: Identifies short-term price direction.
 # Bullish: Price crosses above short-term MA.
 # Bearish: Price crosses below short-term MA.
-def calculate_short_term_MA(df, window=9):
-    pass  # Implement calculation here
+@safe_copy
+def calculate_short_term_MA(df, col='Close', window=10):
+    sma = df[col].rolling(window=window).mean()
+    df[f'SMA_{window}days'] = sma
+    return df
 
 # =============================================
 # Medium-Term Indicators (Weeks to Months)
@@ -96,15 +113,37 @@ def calculate_OBV(df):
 # Meaning: Indicates long-term trend direction.
 # Bullish: Price above 200-day MA.
 # Bearish: Price below 200-day MA.
-def calculate_long_term_MA(df, window=200):
-    pass  # Implement calculation here
+def calculate_long_term_MA(df, col='Close', window=200):
+    lma = df[col].rolling(window=window).mean()
+    df[f'LMA_{window}days'] = lma
+    return df
 
 # Golden Cross / Death Cross (50-day and 200-day MA)
 # Meaning: Signals major shifts in long-term trend.
 # Bullish: 50-day MA crosses above 200-day MA (Golden Cross).
 # Bearish: 50-day MA crosses below 200-day MA (Death Cross).
 def calculate_MA_crossover(df, short_window=50, long_window=200):
-    pass  # Implement calculation here
+    # Find out if moving averages are present in df
+    short_col = f'SMA_{short_window}days'
+    long_col = f'LMA_{long_window}days'
+
+    if short_col not in df.columns:
+        df = calculate_short_term_MA(df, window=short_window)
+
+    if long_col not in df.columns:
+        df = calculate_long_term_MA(df, window=long_window)
+
+    # Crossover logic
+    df[f'MA Crossover ({short_window} vs {long_window})'] = 0
+
+    # Bull = 1 (Golden Cross); Bear = -1 (Death Cross)
+    df.loc[
+        (df[short_col] > df[long_col]) & (df[short_col].shift(1) <= df[long_col].shift(1)),
+        f'MA Crossover ({short_window} vs {long_window})'] = 1
+    df.loc[
+        (df[short_col] < df[long_col]) & (df[short_col].shift(1) >= df[long_col].shift(1)),
+        f'MA Crossover ({short_window} vs {long_window})'] = -1
+    return df
 
 # Accumulation/Distribution Line (A/D)
 # Meaning: Tracks institutional buying or selling pressure.
@@ -120,9 +159,19 @@ def calculate_AD_line(df):
 def get_fundamental_ratios(ticker):
     pass  # Implement retrieval here
 
-# Medium term
+# Chain populate indicators
+def add_technical_indicators(df, short_window=50, long_window=200):
+    return(
+        df
+        # Short term indicators
+        .pipe(calculate_short_term_RSI, col='Close', window=14)
+        .pipe(calculate_short_term_MA, col='Close', window=short_window)
+        # Medium term indicators
+        # Long term indicators
+        .pipe(calculate_long_term_MA, col='Close', window=long_window)
+        .pipe(calculate_MA_crossover, short_window=short_window, long_window=long_window)
+    )
 
-# Long term
 
 def main():
     # Inputs
@@ -132,12 +181,15 @@ def main():
     start_date_input = input('Enter start date (YYYY-MM-DD): ')
     end_date_input = input('Enter end date (YYYY-MM-DD): ')
 
-    start_date = date.fromisoformat(start_date_input) if start_date_input else None
-    end_date = date.fromisoformat(end_date_input) if end_date_input else None
+    start_date, end_date = timeframe_default(start_date=start_date_input, end_date=end_date_input)
 
     df = cmpy.history(start=start_date, end=end_date)
-    print(df.head())
+    # print(df.head())
 
+    # Generate indicators
+    df_with_indicators = add_technical_indicators(df)
+    # Clean up NaN values @@@
+    print(f'\nNew df:\n {df_with_indicators.tail()}')
 
 # Plot key metrics
 if __name__ == "__main__":
